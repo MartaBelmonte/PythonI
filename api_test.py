562d12f8a643933cf5816925ec79e33ef9723e3e
MartaBelmonte <martabelmonts5@gmail.com>
2024-01-09 23:04:53 +0100

    comments

--- api_test.py
+++ api_test.py
@@ -1,16 +1,12 @@
 import requests
 
-# Función para obtener datos de la API y convertirlos a una lista
-
-
 def get_api_data(url):
     try:
-        # solicitud HTTP para obtener el JSON de la URL
         response = requests.get(url)
         response.raise_for_status()
-        json_data = response.json()  # Convertir JSON a un diccionario
-        result = []  # Para que sea una lista
-        for key, value in json_data.items():  # Recorrer el diccionario y agregar cada par clave-valor a la lista
+        json_data = response.json()  
+        result = []  
+        for key, value in json_data.items():  
             result.append((key, value))
         return result
     except requests.exceptions.RequestException as e:
@@ -23,7 +19,7 @@ url_person = "https://randomuser.me/api/"
 url_dog_breed = "https://api.thedogapi.com/v1/breeds/1"
 url_random_data_en = "https://jsonplaceholder.typicode.com/posts/1"
 
-# Obtener datos de la API
+# Obtener datos API
 result_person = get_api_data(url_person)
 result_dog_breed = get_api_data(url_dog_breed)
 result_random_data_en = get_api_data(url_random_data_en)
